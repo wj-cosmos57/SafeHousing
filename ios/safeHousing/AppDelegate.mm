20e306e1cd027118b87e76f1d5802d493042dfa4
jonghokim27 <joey0307@naver.com>
2023-12-17 04:55:43 +0900

    kakao login

--- ios/safeHousing/AppDelegate.mm
+++ ios/safeHousing/AppDelegate.mm
@@ -1,6 +1,8 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <RNKakaoLogins.h>
+
 
 @implementation AppDelegate
 
@@ -21,6 +23,16 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
 }
 
 @end
---
+++ ios/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  safeHousing
+//
+//  Created by 김종호 on 12/17/23.
+//
+
+import Foundation
